8b4f855f52766239149ad1f737c5e2e273c028cb
schitcrafter <vapor.schitcrafter@gmail.com>
2023-02-26 13:44:20 +0100

    🎨 Make sure correct linkme is used

--- mylinkmemacros/src/lib.rs
+++ mylinkmemacros/src/lib.rs
@@ -4,7 +4,7 @@ use quote::quote;
 /// Just adds the linkme::distributed_slice macro
 #[proc_macro_attribute]
 pub fn get(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut dist_slice: TokenStream = quote!(#[linkme::distributed_slice(mylinkmetypes::GET_HANDLERS)]).into();
+    let mut dist_slice: TokenStream = quote!(#[::linkme::distributed_slice(mylinkmetypes::GET_HANDLERS)]).into();
     dist_slice.extend(item);
     dist_slice
 }
