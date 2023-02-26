36c93c151e3302c2eb253a4552f2d23cf049603e
schitcrafter <vapor.schitcrafter@gmail.com>
2023-02-26 14:05:59 +0100

    🐛 Don't use traits as return types anymore

--- mylinkmetypes/src/lib.rs
+++ mylinkmetypes/src/lib.rs
@@ -3,10 +3,22 @@ use std::collections::HashMap;
 
 pub use mylinkmemacros::*;
 
+/// alternative which would work for the user-side, but we can't work with `!Sized` return values.
+/// pub static GET_HANDLERS: [fn(&dyn ToString, HashMap<String, String>) -> dyn HttpResponseTrait] = [..];
+/// 
+/// alternative that doesn't work user-side because we then you wouldn't be able to return objects
+/// owned by the handler function, like `String`s.
+/// pub static GET_HANDLERS: [fn(&dyn ToString, HashMap<String, String>) -> &dyn HttpResponseTrait] = [..];
+/// 
+/// alternative which works, but needs to box everything, and uses `dyn`.
+/// This isn't great, because boxing everything slows it down, and the vtable which is needed for
+/// `dyn HttpResponseTrait` would also mean taking a hit on performance.
+/// pub static GET_HANDLERS: [fn(&dyn ToString, HashMap<String, String>) -> Box<dyn HttpResponseTrait>] = [..];
+/// 
+/// alternative that actually works:
+/// This works, but you need to call .into() at the end of the handler.
 #[distributed_slice]
-pub static GET_HANDLERS: [fn(&dyn ToString, HashMap<String, String>) -> &dyn HttpResponseTrait] = [..];
-// alternative:
-// pub static GET_HANDLERS: [fn(&dyn ToString, HashMap<String, String>) -> HttpResponse] = [..];
+pub static GET_HANDLERS: [fn(&dyn ToString, HashMap<String, String>) -> HttpResponse] = [..];
 
 pub trait HttpResponseTrait {
     fn to_response(&self) -> String;
@@ -15,6 +27,12 @@ pub trait HttpResponseTrait {
 impl HttpResponseTrait for &str {
     fn to_response(&self) -> String {
         self.to_string()
+    }
+}
+
+impl HttpResponseTrait for String {
+    fn to_response(&self) -> String {
+        self.clone()
     }
 }
 
@@ -30,9 +48,14 @@ impl Into<HttpResponse> for &str {
     }
 }
 
+impl Into<HttpResponse> for String {
+    fn into(self) -> HttpResponse {
+        HttpResponse { code: 200, body: self }
+    }
+}
+
 pub fn execute_handlers() {
     for handler in GET_HANDLERS {
-        // let boxed_response = ;
-        println!("Handler return: {:?}", handler(&"body", HashMap::new()).to_response()); 
+        println!("Handler return: {:?}", handler(&"body", HashMap::new()));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use mylinkmetypes::{execute_handlers, get, HttpResponse};
 use std::collections::HashMap;
-use mylinkmetypes::{execute_handlers, get, HttpResponse, HttpResponseTrait};
 
 #[get]
-fn handler_1(input: &dyn ToString, headers: HashMap<String, String>) -> &dyn HttpResponseTrait {
-    println!("From inside handler_1, {:?}, {:?}", input.to_string(), headers);
-    &""
+fn handler_1(input: &dyn ToString, headers: HashMap<String, String>) -> HttpResponse {
+    println!(
+        "From inside handler_1 again, {:?}, {:?}",
+        input.to_string(),
+        headers
+    );
+    let body = input.to_string();
+    body.into()
+    // alternative:
+    // &""
     // alternative:
     // HttpResponse { code: 400, body: "testbody".into() }
 }
